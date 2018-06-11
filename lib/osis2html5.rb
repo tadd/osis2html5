6900a60cacf4ab700521fb08cb23539adc2c73ac
Tadashi Saito <tad.a.digger@gmail.com>
2018-06-11 21:57:47 +0900

    implement simple usage

--- lib/osis2html5.rb
+++ lib/osis2html5.rb
@@ -1,5 +1,15 @@
 require "osis2html5/version"
 
 module Osis2Html5
-  # Your code goes here...
+  module_function
+  def run(*argv)
+    usage(error: false) if argv.size == 0
+    usage(error: true) unless argv.size == 2
+  end
+
+  def usage(error:)
+    out = error ? STDERR : STDOUT
+    out.puts "usage: osis2html5 <input.osis> <output dirname>"
+    exit(!error)
+  end
 end
